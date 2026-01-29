dee96dfddc5998252ec930f3ead6fe9b737d1d9b
carlosnuno <carnuno@ucsc.edu>
2026-01-29 14:15:50 -0800

    basic grapple mechanic done

--- Assets/Scripts/WebShooter.cs
+++ Assets/Scripts/WebShooter.cs
@@ -20,24 +20,82 @@ public class WebShooter : MonoBehaviour
     public float webDamper = 7f;
     public float webMassScale = 4.5f;
     public float webZipStrength = 5f;
+    public float maxDistance;
+    public LayerMask webLayers;
 
     private SpringJoint joint;
     private FixedJoint endJoint;
     private Vector3 webPoint;
     private float distanceFromPoint;
+    private bool webShot;
 
     private void Awake()
     {
         isPressed.action.Enable();
-        isPressed.action.performed += HandleInput;
         InputSystem.onDeviceChange += OnDeviceChange;
         lineRenderer= GetComponent<LineRenderer>();
 
+        webEnd.transform.parent = null;
+
     }
 
-    private void HandleInput(InputAction.CallbackContext context)
+    private void HandleInput()
     {
-        print(isPressed);
+        float triggerValue = isPressed.action.ReadValue<float>();
+        Debug.Log("Trigger value: " + triggerValue);
+
+        if (triggerValue > 0 && !webShot)
+        {
+            webShot = true;
+            ShootWeb();
+        }
+        else if (triggerValue == 0 && webShot)
+        {
+            webShot = false;
+            StopWeb();
+        }
+    }
+
+    private void ShootWeb()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(shooterTip.position, shooterTip.forward, out hit, maxDistance, webLayers))
+        {
+            webPoint = hit.point;
+            webEnd.transform.position = webPoint;
+
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = webPoint;
+
+            distanceFromPoint = Vector3.Distance(player.transform.position, webPoint) * .75f;
+            joint.minDistance = 0;
+            joint.maxDistance = distanceFromPoint;
+
+            joint.spring = webStrength;
+            joint.damper = webDamper;
+            joint.massScale = webMassScale;
+        }
+    }
+
+    private void StopWeb()
+    {
+        Destroy(joint);
+
+        if (endJoint) Destroy(endJoint);
+        lineRenderer.positionCount = 0;
+        
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        if (webShot && joint)
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, shooterTip.position);
+            lineRenderer.SetPosition(1, webEnd.transform.position);
+        }
     }
 
     private void OnDeviceChange(InputDevice device, InputDeviceChange change)
@@ -46,11 +104,9 @@ public class WebShooter : MonoBehaviour
         {
             case InputDeviceChange.Disconnected:
                 isPressed.action.Disable();
-                isPressed.action.performed -=HandleInput;
                 break;
             case InputDeviceChange Reconnected:
                 isPressed.action.Enable();
-                isPressed.action.performed += HandleInput;
                 break;
         }
     }
