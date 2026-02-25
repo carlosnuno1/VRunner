34a028fc29c519ac169c4cf6649576f7f61c97d3
Fishbowl <icecoffee1945@gmail.com>
2026-02-25 13:39:19 -0800

    Minor Updates: restart and controls panel update

---
+++ Assets/UI/Scripts/RestartScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour
+{
+    public void Reset()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
